2b985e96e7ffb0044f31961ce86d0650cfe4b0ff
Ben Townshend <bentownshend@icloud.com>
2017-02-11 13:54:40 +0000

    added test unity scene

--- Unity Project/Assets/Scripts/PlayerInputController.cs
+++ Unity Project/Assets/Scripts/PlayerInputController.cs
@@ -4,13 +4,50 @@ using UnityEngine;
 
 public class PlayerInputController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public SteamVR_TrackedObject trackedObj;
+	public SteamVR_Controller.Device controller {
+		get { return SteamVR_Controller.Input((int)trackedObj.index); }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update() {
+		if (!trackedObj) trackedObj = GetComponent<SteamVR_TrackedObject>();
+
+		GetViveInputs();
+	}
+
+	void GetViveInputs() {
+
+		if (controller.GetAxis() != Vector2.zero)
+		{
+		    Debug.Log(gameObject.name + controller.GetAxis());
+		}
+
+		if (controller.GetHairTriggerDown())
+		{
+		    Debug.Log(gameObject.name + " Trigger Press");
+		}
+
+		if (controller.GetHairTriggerUp())
+		{
+		    Debug.Log(gameObject.name + " Trigger Release");
+		}
+
+		if (controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+		{
+		    Debug.Log(gameObject.name + " Grip Press");
+		}
+
+		if (controller.GetPressUp(SteamVR_Controller.ButtonMask.Grip))
+		{
+		    Debug.Log(gameObject.name + " Grip Release");
+		}
+	}
+
+	void GetControllerInputs() {
+		// TODO Write controller input
+	}
+
+	void GetKeyboardInputs() {
+		// TODO Write keyboard input
 	}
 }
